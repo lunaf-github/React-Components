f841336258aba7feb064ca29a702937d04635e65
Fernando <lunaf91@gmail.com>
2024-01-01 22:47:08 -0800

    convert to typescript

---
+++ src/components/star-rating/StarRating.tsx
@@ -0,0 +1,53 @@
+
+
+
+import React from 'react';
+import {useState} from 'react'
+import './starRating.css';
+import ThemeSelector from '../../providers/theme/ThemeSelector';
+const classNames = require('classnames');
+
+const StarRating = (props: {rating: number, size: string, totalStars:number, lock: boolean}) => {
+
+  const [hover, setHover] = useState(0);
+  const [rating, setRating] = useState(props.rating);
+
+
+  const stars = [];
+
+  for(let i = 1; i <= props.totalStars; i++){
+    const starClass = classNames({
+      'starRating__star': true,
+      'starRating__star--active': i <= (hover || rating)? true: false,
+      'starRating--extraSmall': (props.size === 'extra small')? true : false,
+      'starRating--small': (props.size === 'small')? true : false,
+      'starRating--large': (props.size === 'large')? true : false,
+      'starRating--extraLarge': (props.size === 'extra large')? true : false,
+
+    })
+
+    const star = <button 
+      className={starClass}
+      key={i}
+      onClick={()=> setRating(i)}
+      onMouseEnter={() => setHover(i)}
+      onMouseLeave={() => setHover(rating)}
+      disabled={props.lock}
+    > &#9733; </button>
+
+    stars.push(star)
+  }
+ 
+  return(
+    <div className="starRating">
+      {stars}
+    </div>
+  )
+}
+
+
+StarRating.defaultProps = {
+   lock: false
+}
+
+export default StarRating;
